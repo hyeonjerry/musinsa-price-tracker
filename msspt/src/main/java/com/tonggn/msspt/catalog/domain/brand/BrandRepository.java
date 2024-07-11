1e6ba0f73aa1ce57869294f1ed483d4944c9f39d
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-07-11 21:17:50 +0900

    feat: JpaRepository 추가

---
+++ msspt/src/main/java/com/tonggn/msspt/catalog/domain/brand/BrandRepository.java
@@ -0,0 +1,7 @@
+package com.tonggn.msspt.catalog.domain.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, BrandId> {
+
+}
---
+++ msspt/src/main/java/com/tonggn/msspt/catalog/domain/category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.tonggn.msspt.catalog.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, CategoryId> {
+
+}
